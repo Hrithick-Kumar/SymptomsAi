3c31be78c228534aaa46c3e087a36972cf2a53c1
Hrithick-Kumar <kumarhrithick369@gmail.com>
2026-06-09 11:15:59 +0530

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -1,3 +1,23 @@
 import streamlit as st
+import requests 
 st.header("Symptoms Ai")
+def SymptomsAi(Symptoms):
+  api_url="https://openrouter.ai/api/v1/chat/completions"
+  api_key=st.secrets["SymptomsAi"]
+  headers={
+    "Authorization":f"Bearer{api_key}",
+    "Content-Type":"application/json"
+  }
+  payload={
+    "model":"openrouter/free",
+    "messages":[{"role":"system","content":"you are an expert general doctor.you can analyze symptoms and show possibilities"},
+                {"role":"user","content":Symptoms}]
+  }
+  response=requests.post(api_url,headers=headers,json=payload)
+  result=response.json()
+  return result
+Symptoms=st.chat_input("Ask Doctor")
+answer=SymptomsAi(Symptoms)
+st.markdown(answer)
+
 
